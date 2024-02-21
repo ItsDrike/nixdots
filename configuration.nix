765c01e8f3d6c850257b2d23ae9cc5a424b8e285
ItsDrike <itsdrike@protonmail.com>
2024-02-21 21:35:02 +0100

    Add gnupg

--- configuration.nix
+++ configuration.nix
@@ -64,6 +64,7 @@
   environment.systemPackages = with pkgs; [
   vim
   git
+  gnupg
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];

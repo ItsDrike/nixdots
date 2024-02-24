63a3e84edaf7d1c62fb9e308c7341851b606d25b
ItsDrike <itsdrike@protonmail.com>
2024-02-24 21:56:31 +0100

    Fix indentation

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
-    description = "ItsDrike's NixOS configuration";
+  description = "ItsDrike's NixOS configuration";
 
-    inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-    outputs = { self, nixpkgs, ...} @ inputs: {
-        nixosConfigurations = {
-            nixos = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-                modules = [ ./configuration.nix ];
-            };
-        };
+  outputs = { self, nixpkgs, ...} @ inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
     };
+  };
 }

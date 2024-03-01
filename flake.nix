58d9a3c90819558dc50fd583b67d04f43f15b066
ItsDrike <itsdrike@protonmail.com>
2024-03-01 14:42:26 +0100

    Add home-manager

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {self, nixpkgs, ...} @ inputs: let

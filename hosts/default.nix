442d00a82a2560eeeced6e90851ae444f4b42f6d
ItsDrike <itsdrike@protonmail.com>
2024-03-21 22:33:38 +0100

    Declare shared modules

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,13 +1,17 @@
 {self, inputs, ...}: let
   inherit (inputs.nixpkgs) lib;
+
+  # A list of shared modules that ALL systems need
+  shared = [
+    ../system
+    ../options
+  ];
 in {
   vboxnix = lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
       ./vbox_nix
-      ../system
-      ../options
       inputs.home-manager.nixosModules.home-manager
-    ];
+    ] ++ shared;
   };
 }

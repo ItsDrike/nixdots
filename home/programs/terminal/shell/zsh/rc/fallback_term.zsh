c142161840af16246a71ba3fe4e688e5684be4b5
ItsDrike <itsdrike@protonmail.com>
2024-03-25 23:41:19 +0100

    Don't set local var (remains set)

--- home/programs/terminal/shell/zsh/rc/fallback_term.zsh
+++ home/programs/terminal/shell/zsh/rc/fallback_term.zsh
@@ -2,7 +2,6 @@
 # fall back to xterm. This fixes SSH connections from unknown terminals
 
 if ! infocmp "$TERM" &>/dev/null; then
-  local original="$TERM"
+  echo "Setting \$TERM to xterm-256color due to missing terminfo entry for $TERM."
   export TERM=xterm-256color
-  echo "TERM set to $TERM due to missing terminfo entry for $original."
 fi

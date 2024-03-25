d62ad7350fcda3b4c17d3f19aac3c518baf60541
ItsDrike <itsdrike@protonmail.com>
2024-03-25 23:29:09 +0100

    Fall back to xterm-256color instead

--- home/programs/terminal/shell/zsh/rc/fallback_term.zsh
+++ home/programs/terminal/shell/zsh/rc/fallback_term.zsh
@@ -1,9 +1,8 @@
 # If the set $TERM variable doesn't match any configured terminfo entries
 # fall back to xterm. This fixes SSH connections from unknown terminals
 
-if [ -z "$TERM" ]; then
-  export TERM=xterm
-elif ! infocmp "$TERM" &>/dev/null; then
-  export TERM=xterm
-  echo "TERM set to xterm due to missing terminfo entry."
+if ! infocmp "$TERM" &>/dev/null; then
+  local original="$TERM"
+  export TERM=xterm-256color
+  echo "TERM set to $TERM due to missing terminfo entry for $original."
 fi

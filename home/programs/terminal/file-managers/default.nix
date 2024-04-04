c5296ed6f840adf1073032ceb7b129d91c44b772
ItsDrike <itsdrike@protonmail.com>
2024-04-05 01:25:59 +0200

    Add vifm (terminal filemgr)

--- home/programs/terminal/file-managers/default.nix
+++ home/programs/terminal/file-managers/default.nix
@@ -1,7 +1,12 @@
+# Note: Pretty much all of these configurations were copied from 
+# https://git.notashelf.dev/NotAShelf/nyx, there may be some small
+# adjustments made to them but the majority of the credit goes to
+# notashelf.
 _: {
   imports = [
     ./yazi
     ./xplr
+    ./vifm
   ];
 }
 

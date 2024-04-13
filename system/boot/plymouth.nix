31221a5d1916f33d8be36039a3edbb693b5c8b84
ItsDrike <itsdrike@protonmail.com>
2024-04-13 18:14:32 +0200

    Add splash kernel param if plymouth is enabled

--- system/boot/plymouth.nix
+++ system/boot/plymouth.nix
@@ -3,16 +3,20 @@
   cfg = config.myOptions.system.boot.plymouth;
 in {
   config = mkIf cfg.enable {
-    boot.plymouth = {
-      enable = true;
-      theme = cfg.selectedTheme;
-    }
-    // lib.optionalAttrs cfg.withThemes {
-      themePackages = [
-        (pkgs.adi1090x-plymouth-themes.override {
-          selected_themes = [ cfg.selectedTheme ];
-        })
-      ];
+    boot = {
+      plymouth = {
+        enable = true;
+        theme = cfg.selectedTheme;
+      }
+      // lib.optionalAttrs cfg.withThemes {
+        themePackages = [
+          (pkgs.adi1090x-plymouth-themes.override {
+            selected_themes = [ cfg.selectedTheme ];
+          })
+        ];
+      };
+
+      kernelParams = ["splash"];
     };
 
     # Make polymouth work with sleep

e47f41a3c05ee20399991fed9bac5b77256e90bb
ItsDrike <itsdrike@protonmail.com>
2024-04-18 16:39:39 +0200

    Explicitly disable nano

--- system/shared/programs.nix
+++ system/shared/programs.nix
@@ -1,10 +1,17 @@
-{
+{lib, ...}: let
+  inherit (lib) mkForce;
+in {
 
-  # Install an actually usable system-wide editor
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    vimAlias = true;
-    viAlias = true;
+  programs = {
+    # Explicitly disable nano, it sucks and I don't want it
+    nano.enable = mkForce false;
+
+    # Install an actually usable system-wide editor
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+      viAlias = true;
+    };
   };
 }

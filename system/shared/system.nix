130fdae4bffd5f726ed90c0b17213e4275792dbb
ItsDrike <itsdrike@protonmail.com>
2024-05-15 20:40:16 +0200

    Remove mutableUsers setting from system.nix (it's in impermanence)

--- system/shared/system.nix
+++ system/shared/system.nix
@@ -8,13 +8,6 @@ in
   programs.zsh.enable = true;
 
   users = {
-    # Prevent mutating users outside of our configurations.
-    # TODO: Solve this, currentry it fails with no password
-    # specified for root account nor any whell user accounts
-    # and wants us to set pw manually with passwd, which needs
-    # mutableUsers
-    #mutableUsers = false;
-
     users.${cfg.username} = {
       isNormalUser = true;
       extraGroups = [ "wheel" ];

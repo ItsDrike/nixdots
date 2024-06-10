4c775478bc7fa9d2bbf80c31ce0880fe1b2cc06b
ItsDrike <itsdrike@protonmail.com>
2024-06-10 18:52:38 +0200

    Add missing device role check for physlock

--- system/roles/workstation/programs/physlock.nix
+++ system/roles/workstation/programs/physlock.nix
@@ -1,8 +1,14 @@
-{
-  # Screen locker which works across all virtual terminals
-  # Use `systemctl start physlock` to securely lock the screen
-  services.physlock = {
-    enable = true;
-    lockMessage = "System is locked...";
+{ lib, config, ...}: let
+  inherit (lib) mkIf;
+  deviceType = config.myOptions.device.roles.type;
+  acceptedTypes = ["laptop" "desktop"];
+in {
+  config = mkIf (builtins.elem deviceType acceptedTypes) {
+    # Screen locker which works across all virtual terminals
+    # Use `systemctl start physlock` to securely lock the screen
+    services.physlock = {
+      enable = true;
+      lockMessage = "System is locked...";
+    };
   };
 }

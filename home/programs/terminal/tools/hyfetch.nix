5df0ffe4e56c58d98a8cc9a272d2de3f89f85bf4
ItsDrike <itsdrike@protonmail.com>
2024-06-20 16:09:01 +0200

    Add hyfetch settings

--- home/programs/terminal/tools/hyfetch.nix
+++ home/programs/terminal/tools/hyfetch.nix
@@ -1,3 +1,21 @@
 {
-  programs.hyfetch.enable = true;
+  programs.hyfetch = {
+    enable = true;
+    settings = {
+      preset = "rainbow";
+      mode = "rgb";
+      light_dark = "dark";
+      lightness = 0.65;
+      color_align = {
+        mode = "horizontal";
+        custom_colors = [];
+        force_back = null;
+      };
+      backend = "neofetch";
+      args = null;
+      distro = null;
+      pride_month_shown = [];
+      pride_month_disable = true;
+    };
+  };
 }

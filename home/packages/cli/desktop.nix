d8513f4cdca3e5c4f930a4c66e65966fab39ff1f
ItsDrike <itsdrike@protonmail.com>
2024-06-21 12:36:48 +0200

    Add more cli programs

--- home/packages/cli/desktop.nix
+++ home/packages/cli/desktop.nix
@@ -11,8 +11,16 @@
 in {
   config = mkIf (builtins.elem devType acceptedTypes) {
     home.packages = with pkgs; [
-      trash-cli
-      bitwarden-cli
+      libnotify # send desktop notifications
+      imagemagick # create/edit images
+      trash-cli # interface to freedesktop trashcan
+      bitwarden-cli # pw manager
+      slides # terminal based presentation tool
+      brightnessctl # brightness control
+      pulsemixer # manage audio (TUI)
+      nix-tree # interactively browse nix store
+      glow # render markdown
+      ffmpeg # record, convert and stream audio and video
     ];
   };
 }

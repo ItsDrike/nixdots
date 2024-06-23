d3d8ad15b33ba6983ebff8935ac00941637d56ca
ItsDrike <itsdrike@protonmail.com>
2024-06-23 14:59:10 +0200

    Add default portal to home-manager config

--- home/programs/xdg/portal.nix
+++ home/programs/xdg/portal.nix
@@ -4,5 +4,11 @@
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
     ];
+    # Specify which portals should be used by the individual interfaces
+    # see: <https://github.com/flatpak/xdg-desktop-portal/blob/1.18.1/doc/portals.conf.rst.in>
+    config.common = {
+      # Use this portal for every interface, unless a specific override is present
+      default = ["gtk"];
+    };
   };
 }

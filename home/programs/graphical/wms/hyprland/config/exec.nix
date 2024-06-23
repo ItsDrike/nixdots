a0b525dc162178d0eb3a7e01ff0c0fabb629db43
ItsDrike <itsdrike@protonmail.com>
2024-06-23 14:53:29 +0200

    Import wayland vars to systemd & dbus on hyprland start

--- home/programs/graphical/wms/hyprland/config/exec.nix
+++ home/programs/graphical/wms/hyprland/config/exec.nix
@@ -1,8 +1,8 @@
-{
+{pkgs, ...}: {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      # TODO: Add this (probably only vesktop)
+      "${pkgs.systemd}/bin/systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+      "${pkgs.systemd}/bin/dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=$XDG_CURRENT_DESKTOP"
     ];
   };
 }
-

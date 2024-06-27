4660bc004601c3592f1f3040810bfb8d25760973
ItsDrike <itsdrike@protonmail.com>
2024-06-27 15:42:37 +0200

    hyprland: dbus/systemd import some more theme related variables

--- home/programs/graphical/wms/hyprland/config/exec.nix
+++ home/programs/graphical/wms/hyprland/config/exec.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "${pkgs.systemd}/bin/systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-      "${pkgs.systemd}/bin/dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=$XDG_CURRENT_DESKTOP"
+      "${pkgs.systemd}/bin/systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP GTK_THEME QT_QPA_PLATFORMTHEME QT_STYLE_OVERRIDE"
+      "${pkgs.systemd}/bin/dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP GTK_THEME QT_QPA_PLATFORMTHEME QT_STYLE_OVERRIDE"
     ];
   };
 }

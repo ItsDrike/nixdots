ee9c647cb31e8d4c98ef4a5856988b7d0ff82ee1
ItsDrike <itsdrike@protonmail.com>
2024-06-27 22:12:27 +0200

    ssh: update paths to some identity files

--- home/programs/terminal/tools/ssh.nix
+++ home/programs/terminal/tools/ssh.nix
@@ -13,12 +13,12 @@
         "gitlab" = {
           user = "git";
           hostname = "gitlab.com";
-          identityFile = "~/.ssh/git/gitlab";
+          identityFile = "~/.ssh/git/gitlab-itsdrike";
         };
         "github" = {
           user = "git";
           hostname = "gitlab.com";
-          identityFile = "~/.ssh/git/github";
+          identityFile = "~/.ssh/git/github-itsdrike";
         };
         # TODO: Figure out how to add protected/encrypted blocks here
         # I don't like the idea of expising IPs/hostnames in the config

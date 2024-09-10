161b754274d4ca1ed027fe0383f95c5ad68634c7
ItsDrike <itsdrike@protonmail.com>
2024-09-10 15:32:35 +0200

    Use native nix packages in neovim

--- home/programs/terminal/editors/neovim/default.nix
+++ home/programs/terminal/editors/neovim/default.nix
@@ -1,26 +1,57 @@
 {pkgs, ...}: {
-  # TODO: Consider switching to nixvim
-
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
 
     # Packages required for:
+    #
     # - Mason to build various language servers / linters / formatters from source
     # - Runtime dependencies of plugins / lang servers / ...
     withNodeJs = true;
     withPython3 = true;
     extraPackages = with pkgs; [
+      # Tools for building Mason packages
       go
       python3
       rustc
       cargo
       gcc
+      cmake
+      gnumake
+
+      # lang servers
+      lua-language-server
+      rust-analyzer
+      taplo
+      gopls
+      marksman
+      yaml-language-server
+      ruff
+      neocmakelsp
+      bash-language-server
+      nixd
+
+      # Linters / formatters
+      stylua
+      shfmt
+      gofumpt
+      gotools
+      sqlfluff
+      hadolint
+      markdownlint-cli2
+      nodePackages.prettier
+      shellcheck
+      shfmt
+      alejandra
       deadnix
       statix
-      alejandra
-      nixd
+
+      # Other tools / utilities
+      ripgrep
+      fd
+      jq
+      lazygit
     ];
   };
 }
